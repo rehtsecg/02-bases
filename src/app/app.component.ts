c619106fb4d3185bca0638c6737450d574655906
rehtsecg <rehtsecg@gmail.com>
2025-01-20 20:38:32 +0100

    botones y contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title :string  = 'Hola mundo';
   public counter: number = 10;
+
+  increaseby(value:number):void{
+
+     this.counter+=value;
+
+  }
+  resetCounter(){
+
+    this.counter= 10;
+  }
 }
