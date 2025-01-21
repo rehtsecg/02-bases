323658afc9543c474c78f403fe3e62afbb210260
rehtsecg <rehtsecg@gmail.com>
2025-01-21 14:02:13 +0100

    cambiar nombre y tarea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  public title :string  = 'Hola mundo';
+  public title :string  = 'Hola mundo ';
 
 }
---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+  public name:string="ironman";
+  public age:number=45;
+
+ get capitalizaName():string{
+  return this.name.toUpperCase();
+
+ }
+ getHeroDescription():string{  // metodo
+
+   return `${this.name} - ${this.age}`;
+   //return this.name +'-'+this.age.toString() ;
+ }
+ changeHero():void{
+     this.name='Spiderman';
+ }
+
+
+ changeAge():void{
+   this.age= 25;
+ }
+}
